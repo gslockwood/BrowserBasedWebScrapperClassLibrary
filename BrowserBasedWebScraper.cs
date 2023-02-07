f6a8368109133b60784e03c814bec0db122b26ff
George S Lockwood <George.S.Lockwood@gmail.com>
2023-02-07 10:33:44 -0800

    Initial commit

---
+++ BrowserBasedWebScraper.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrowserBasedWebScrapperClassLibrary
+{
+    public class BrowserBasedWebScraper
+    {
+        public static Task<string> LoadUrl(string url)
+        {
+            var tcs = new TaskCompletionSource<string>();
+            Thread thread = new Thread(() =>
+            {
+                try
+                {
+                    Func<string> f = () =>
+                    {
+                        using (WebBrowser browser = new WebBrowser())
+                        {
+                            browser.ScriptErrorsSuppressed = true;
+                            browser.Navigate(url);
+                            //int maxCount = 1500;
+                            int counter = 0;
+                            //while (browser.ReadyState != System.Windows.Forms.WebBrowserReadyState.Complete && counter < maxCount)
+                            while (browser.ReadyState != WebBrowserReadyState.Complete)
+                            {
+                                System.Windows.Forms.Application.DoEvents();
+                                counter++;
+                            }
+                            return browser.DocumentText;
+                        }
+                    };
+                    tcs.SetResult(f());
+                }
+                catch (Exception e)
+                {
+                    tcs.SetException(e);
+                }
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.IsBackground = true;
+            thread.Start();
+            return tcs.Task;
+        }
+    }
+
+}
